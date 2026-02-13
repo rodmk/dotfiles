8530d27cd5bdab3f468aa5e6fa5b7743adf76de5
Rodrigo Muñoz <2280164+rodmk@users.noreply.github.com>
2026-02-13 06:39:40 +0000

    fix et

--- lib/pre/et.sh
+++ lib/pre/et.sh
@@ -3,5 +3,5 @@
 # apt: et
 [ "$(uname)" = "Darwin" ] && exit 0
 command -v et >/dev/null && exit 0
-sudo apt-get install -y software-properties-common
+command -v add-apt-repository >/dev/null || { sudo apt-get update && sudo apt-get install -y software-properties-common; }
 sudo add-apt-repository -y --no-update ppa:jgmath2000/et
