79adbfd4f1c1747ae2a120ab6dcca6c6e15d05ed
Rodrigo Muñoz <2280164+rodmk@users.noreply.github.com>
2026-02-15 05:20:46 +0000

    drop et

--- lib/pkg/et.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck shell=bash
-brew_packages() { echo "MisterTea/et/et"; }
-apt_packages() { echo "et"; }
-apt_repo() {
-	command -v add-apt-repository >/dev/null || { sudo apt-get update && sudo apt-get install -y software-properties-common; }
-	sudo add-apt-repository -y --no-update ppa:jgmath2000/et
-}
