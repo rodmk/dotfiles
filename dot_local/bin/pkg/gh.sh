0cd557a035a1c1e79abf33f3ecf855176011604a
Rodrigo Muñoz <2280164+rodmk@users.noreply.github.com>
2026-03-11 03:53:19 +0000

    jq + update gh

--- dot_local/bin/pkg/gh.sh
+++ dot_local/bin/pkg/gh.sh
@@ -1,2 +1,10 @@
 # shellcheck shell=bash
+apt_repo() {
+	sudo mkdir -p -m 755 /etc/apt/keyrings
+	wget -nv -O- https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+		| sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
+	sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
+		| sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+}
 packages() { echo "gh"; }
