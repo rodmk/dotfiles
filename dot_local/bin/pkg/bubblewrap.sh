4bf8be890568df254438fbfd807ff331d347caaf
Rodrigo Muñoz <2280164+rodmk@users.noreply.github.com>
2026-07-18 14:09:34 -0700

    fix ci

--- dot_local/bin/pkg/bubblewrap.sh
+++ dot_local/bin/pkg/bubblewrap.sh
@@ -2,8 +2,11 @@
 apt_packages() { echo "apparmor-profiles apparmor-utils bubblewrap"; }
 
 apt_install() {
-	sudo install -m 0644 \
-		/usr/share/apparmor/extra-profiles/bwrap-userns-restrict \
-		/etc/apparmor.d/bwrap-userns-restrict
-	sudo apparmor_parser -r /etc/apparmor.d/bwrap-userns-restrict
+	[ "${CODESPACES:-}" = true ] && return
+	local extra_profile=/usr/share/apparmor/extra-profiles/bwrap-userns-restrict
+	local active_profile=/etc/apparmor.d/bwrap-userns-restrict
+	if [ -f "$extra_profile" ]; then
+		sudo install -m 0644 "$extra_profile" "$active_profile"
+		sudo apparmor_parser -r "$active_profile"
+	fi
 }
